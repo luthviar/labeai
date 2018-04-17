fada4754e6757483e0671df6eedf6fb045074e7f
Hawari Hilman <hawarihilman@gmail.com>
2018-04-17 14:09:11 +0800

    nambahin lib nusoap sama bikin halaman buat testing soap

---
+++ testingsoap.php
@@ -0,0 +1,13 @@
+<?php
+require_once('lib/nusoap.php');
+$client = new nusoap_client('http://tempuri.org/');
+/*
+echo "test <br/>";
+$data = array(
+	'intA'=>"1",
+	'intB'=> "3"
+);
+*/
+$response = $client->call('add', array('intA'=> 1, 'intB'=> 3));
+echo $response;
+?>
